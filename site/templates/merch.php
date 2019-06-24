43504e3abdd265c858bc52360b86b243469d0e13
Bastian Allgeier <mail@bastianallgeier.com>
2019-06-24 17:34:28 +0200

    Updated styles

--- site/templates/merch.php
+++ site/templates/merch.php
@@ -1,29 +1,17 @@
-<?php snippet('header') ?>
+<?php snippet('header', ['background' => 'dark']) ?>
 
-  <main class="default-page | main" id="maincontent">
-    <article class="wrap">
-      <?php snippet('hero', ['align' => 'center']) ?>
-      <div class="text">
-        <?= $page->text()->kt()->anchorHeadlines() ?>
+  <main class="merch-page | main" id="maincontent">
+    <div class="wrap">
+      <?php snippet('hero', ['theme' => 'dark']) ?>
+    </div>
+
+    <div class="kirbymerch-wrapper">
+      <div id="kirbymerch" class="wrap">
+        <a href="https://shop.spreadshirt.de/getkirby">
+          Loading shop …
+        </a>
       </div>
-    </article>
+    </div>
   </main>
 
-<div id="kirbymerch" class="wrap">
-  <a href="https://shop.spreadshirt.de/getkirby">
-    Loading shop …
-  </a>
-</div>
-
-<!-- <script>
-    var spread_shop_config = {
-        shopName: 'getkirby',
-        locale: 'en_DE',
-        prefix: 'https://shop.spreadshirt.de',
-        baseId: 'kirbyshop'
-    };
-</script>
-
-<script src="https://shop.spreadshirt.de/shopfiles/shopclient/shopclient.nocache.js"></script> -->
-
-<?php snippet('footer') ?>
+<?php snippet('footer', ['theme' => 'dark']) ?>
